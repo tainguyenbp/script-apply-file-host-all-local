f472fe92ef83e97a08b662abe83463471d5c7938
root <root@localhost.localdomain>
2018-09-28 23:51:11 +0700

    Done script

--- script-apply-file-host.sh
+++ script-apply-file-host.sh
@@ -23,23 +23,36 @@ echo "$CURRENT_DIR"
 PATH_FILE_HOSTS="$CURRENT_DIR/hosts"
 PATH_FILE_SERVER="$CURRENT_DIR/server-virtual-linux.csv"
 
+function delete_line_empty_file_csv(){
+
+		sed -i /^$/d "$PATH_FILE_SERVER"
+	
+}
+
 function check_file_and_run_script() {
 	
-	if [ -f "$PATH_FILE_HOSTS" ] && [ -f "$PATH_FILE_SERVER" ]
+	if [ -f "$PATH_FILE_SERVER" ] && [ -f "$PATH_FILE_SERVER" ]
 		then
 			echo "File $PATH_FILE_HOSTS found !!!"
 			echo "File $PATH_FILE_SERVER found !!!"
-		 while IFS=',' read -r ip_host username password port description i line 
+		# call function delete_line_empty_file_csv
+ 		delete_line_empty_file_csv
+		value_line_csv=0
+		while IFS=',' read -r ip_host username password port description;
 			do
-				echo "Ip host : $ip_host"
-				echo "Username : $username"
-				echo "Password : $password"
-				echo "Port ssh : $port"
-				echo "Description : $description"
+				if [ $value_line_csv != 0 ]
+					then
+						echo "Ip host : $ip_host"
+						echo "Username : $username"
+						echo "Password : $password"
+						echo "Port ssh : $port"
+						echo "Description : $description"
 			
-			sshpass -p"$password" scp -r -P"$port" -o StrictHostKeyChecking=no "$PATH_FILE_HOSTS" "$username"@"$ip_host":/etc/
+					#	sshpass -p"$password" scp -r -P"$port" -o StrictHostKeyChecking=no "$PATH_FILE_HOSTS" "$username"@"$ip_host":/etc/
 	
-			echo "============================== Done Virtual ===================================================================="	
+						echo "============================== Done Virtual ===================================================================="	
+				fi
+				value_line_csv=$value_line_csv+1
 			
 		done < "$PATH_FILE_SERVER"
 		
